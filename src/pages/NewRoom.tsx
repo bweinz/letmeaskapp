f29a64d986dd261cf49ed2ef992b9b74ac075c78
Bruno Weinz <82482996+bweinz@users.noreply.github.com>
2021-09-20 14:01:28 -0300

    Adicionando Elementos

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,46 @@
+export function NewRoom() {
+return (
+
+);
+}
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/delete.svg';
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+//webpack (snopack, vite, ...)
+
+//Module Bundler -> todas as importações como a illustration ele vê que é um svg e decide qual formato ele vai transformar a imagem, pega a extensão do arquivo e tem uma pré definição para ativar dentro do codigo
+
+export function NewRoom() {
+    return ( 
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie sala de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie uma sala com o Google
+                    </button>
+                    <div className="separator"> ou entre em uma sala</div>
+                    <form>
+                        <input
+                        type="text" 
+                        placeholder="Digite o código da sala"
+                        />
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
